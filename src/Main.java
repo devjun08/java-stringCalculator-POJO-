adda9f48f35b3500fa97c503a914180115309979
KimJunYeop <devjun08@gmail.com>
2025-11-27 00:22:42 +0900

    Refactor: 인풋, 계산, 아웃풋 구조로 변경

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,12 @@
-import java.util.List;
-import java.util.Scanner;
+import input.Input;
+import output.Output;
+import calculator.Calculator;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        try {
-            System.out.println("Input:");
-            String input = scanner.nextLine();
-
-            // 1) 파싱
-            Parser parser = new Parser();
-            List<Integer> parsedData = parser.parseInput(input);
-
-            // 2) 검증
-            Validator validator = new Validator();
-            validator.validate(parsedData);
-
-            // 3) 계산
-            Calculator calculator = new Calculator();
-            int result = calculator.calculate(parsedData);
-
-            // 4) 결과 출력
-            System.out.println("Result: " + result);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Error: " + e.getMessage());
-        }
+        String userInput = Input.nextLine();
+        calculator calculator = new Calculator();
+        Output output = new Output();
     }
 }
